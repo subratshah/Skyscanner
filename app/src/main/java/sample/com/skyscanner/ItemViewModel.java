120d801e13f65c13655f48768b92ee9172c303b6
Subrat Shah <subratshah@users.noreply.github.com>
2019-07-22 15:59:13 +0530

    Recycelr View

--- app/src/main/java/sample/com/skyscanner/ItemViewModel.java
+++ app/src/main/java/sample/com/skyscanner/ItemViewModel.java
@@ -1,4 +1,29 @@
 package sample.com.skyscanner;
 
-class ItemViewModel {
+
+import androidx.databinding.BaseObservable;
+
+import retrofit2.Retrofit;
+import sample.com.skyscanner.Network.RetrofitUtil;
+import sample.com.skyscanner.Network.WeatherService;
+
+public class ItemViewModel extends BaseObservable {
+    Retrofit retrofit = RetrofitUtil.getRetrofitInstance();
+    WeatherService weatherService = retrofit.create(WeatherService.class);
+
+    public ItemViewModel() {
+//        Call<sample.com.skyscanner.Objects.Model> call = weatherService.getWeather();
+//        call.enqueue(new Callback<sample.com.skyscanner.Objects.Model>() {
+//
+//            @Override
+//            public void onResponse(Call<sample.com.skyscanner.Objects.Model> call, Response<sample.com.skyscanner.Objects.Model> response) {
+//                if (response.body() != null) {
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<sample.com.skyscanner.Objects.Model> call, Throwable t) {
+//            }
+//        });
+    }
 }
--- app/src/main/java/sample/com/skyscanner/RecyclerAdapter.java
+++ app/src/main/java/sample/com/skyscanner/RecyclerAdapter.java
@@ -1,54 +1,42 @@
 package sample.com.skyscanner;
 
-import android.databinding.DataBindingUtil;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.weatherapp.databinding.RecyclerviewItemBinding;
-
-import java.util.List;
-
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
-
-    private List<ItemViewModel> itemViewModels;
-
-    RecyclerAdapter(List<ItemViewModel> itemViewModels) {
-        this.itemViewModels = itemViewModels;
-    }
-
-    @NonNull
-    @Override
-    public RecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        RecyclerviewItemBinding recyclerviewItemBinding = DataBindingUtil.inflate(LayoutInflater.from(viewGroup.getContext()), R.layout.recyclerview_item, viewGroup, false);
-        return new ViewHolder(recyclerviewItemBinding);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerAdapter.ViewHolder viewHolder, int i) {
-        ItemViewModel itemViewModel = itemViewModels.get(i);
-        viewHolder.bind(itemViewModel);
-    }
-
-    @Override
-    public int getItemCount() {
-        return itemViewModels.size();
-    }
-
-    public class ViewHolder extends RecyclerView.ViewHolder {
-
-        RecyclerviewItemBinding recyclerviewItemBinding;
-
-        public ViewHolder(@NonNull RecyclerviewItemBinding recyclerviewItemBinding) {
-            super(recyclerviewItemBinding.getRoot());
-            this.recyclerviewItemBinding = recyclerviewItemBinding;
-        }
-
-        public void bind(ItemViewModel itemViewModel) {
-            recyclerviewItemBinding.setItemViewModel(itemViewModel);
-        }
-    }
+public class RecyclerAdapter {
+//
+//    private List<ItemViewModel> itemViewModels;
+//
+//    RecyclerAdapter(List<ItemViewModel> itemViewModels) {
+//        this.itemViewModels = itemViewModels;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public RecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+//        RecyclerviewItemBinding recyclerviewItemBinding = DataBindingUtil.inflate(LayoutInflater.from(viewGroup.getContext()), R.layout.recyclerview_item, viewGroup, false);
+//        return new ViewHolder(recyclerviewItemBinding);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull RecyclerAdapter.ViewHolder viewHolder, int i) {
+//        ItemViewModel itemViewModel = itemViewModels.get(i);
+//        viewHolder.bind(itemViewModel);
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return itemViewModels.size();
+//    }
+//
+//    public class ViewHolder extends RecyclerView.ViewHolder {
+//
+//        RecyclerviewItemBinding recyclerviewItemBinding;
+//
+//        public ViewHolder(@NonNull RecyclerviewItemBinding recyclerviewItemBinding) {
+//            super(recyclerviewItemBinding.getRoot());
+//            this.recyclerviewItemBinding = recyclerviewItemBinding;
+//        }
+//
+//        public void bind(ItemViewModel itemViewModel) {
+//            recyclerviewItemBinding.setItemViewModel(itemViewModel);
+//        }
+//    }
 }
