e45a0d5880365e3b3a1b30d6d19e8ac0cc25906d
Subrat Shah <subrat.s94@gmail.com>
2019-08-02 17:14:34 +0530

    Added Calendar View

--- app/src/main/java/sample/com/skyscanner/ui/MainActivity.java
+++ app/src/main/java/sample/com/skyscanner/ui/MainActivity.java
@@ -1,11 +1,13 @@
 package sample.com.skyscanner.ui;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,33 +16,56 @@ import androidx.lifecycle.LifecycleOwner;
 
 import javax.inject.Inject;
 
-import sample.com.skyscanner.viewmodels.MainViewModel;
 import sample.com.skyscanner.R;
 import sample.com.skyscanner.databinding.ActivityMainBinding;
+import sample.com.skyscanner.viewmodels.MainViewModel;
 
-public class MainActivity extends AppCompatActivity implements LifecycleOwner, AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements LifecycleOwner, DatePickerDialog.OnDateSetListener, AdapterView.OnItemSelectedListener {
 
     @Inject
     MainViewModel mainViewModel;
 
-    Spinner spinner;
+    ActivityMainBinding activityMainBinding;
     Intent intent;
     String[] currencies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
         setTitle("Enter Flight Dertails");
-        intent = new Intent(this, SecondActivity.class);
-        spinner = findViewById(R.id.currSpinner);
-        currencies = new String[]{"INR", "USD"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, currencies);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        activityMainBinding.setViewModel(mainViewModel);
+        intent = new Intent(this, SecondActivity.class);
+        currencies = new String[]{"INR", "USD"};
+
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, currencies);
+        activityMainBinding.currSpinner.setAdapter(spinnerAdapter);
+        activityMainBinding.currSpinner.setOnItemSelectedListener(this);
+    }
+
+    public void onClickDate(View view) {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+        activityMainBinding.dateText.setText(i + "-" + String.format("%02d", i1) + "-" + String.format("%02d", i2));
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        intent.putExtra("curr", currencies[position]);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 
     public void onClickScan(View view) {
@@ -61,12 +86,4 @@ public class MainActivity extends AppCompatActivity implements LifecycleOwner, A
 
         startActivity(intent);
     }
-
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-        intent.putExtra("curr", currencies[position]);
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) { }
 }
